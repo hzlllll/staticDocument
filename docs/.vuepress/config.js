a3e8792d81957edf77a5819eb760907ac415826a
huzl <wx_6aa66b32ddb84865a24c793e28a72866@git.code.tencent.com>
2021-09-18 18:08:53 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: '进化树说明文档！',
+    description: '这是我的第一个 VuePress 站点',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+    plugins: [
+        [
+            '@vuepress/plugin-search',
+            {
+                getExtraFields: (page) => page.
+                    locales: {
+                '/': {
+                    placeholder: 'Search',
+                },
+                '/zh/': {
+                    placeholder: '搜索',
+                },
+            },
+            },
+        ],
+    ],
+}
+
